78a44139baef52a9f4a28326a9d7201c3fe47692
Unknown8991 <rafal.pat@onet.pl>
2021-09-24 16:35:09 +0200

    Updates

---
+++ precache-manifest.0186004021bf592f52d7d5ccc91b91e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1bb5dbc8e76f93cf83d6",
+    "url": "/WeatherApp/static/css/main.32895819.chunk.css"
+  },
+  {
+    "revision": "1bb5dbc8e76f93cf83d6",
+    "url": "/WeatherApp/static/js/main.c5e3d637.chunk.js"
+  },
+  {
+    "revision": "4efbdecc212de1558e2a",
+    "url": "/WeatherApp/static/js/runtime~main.3044ef82.js"
+  },
+  {
+    "revision": "286d30abf3ea5d4c38ee",
+    "url": "/WeatherApp/static/js/2.42199f77.chunk.js"
+  },
+  {
+    "revision": "1009d5b2ecd4c94fb6cfd8014e2780a1",
+    "url": "/WeatherApp/index.html"
+  }
+];
